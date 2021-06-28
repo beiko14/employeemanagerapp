ea96b6de2ee8c967861fcdeab76fbb6cc6a6f527
Beiko14 <heiko.hollerith14@gmx.de>
2021-06-28 12:31:33 +0200

    angular start files created employee.ts interface created employee.service.ts to create HttpRequests updated app.components.ts updated app.component.html updated environment.ts updated.module.ts to activate HttpClientModule

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+
+// use service in component
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  public employees: Employee[] | undefined;
+
+  constructor(private employeeService: EmployeeService) {}
+
+  // run getEmployees() whenever this component is initialized
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void{
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
+}
